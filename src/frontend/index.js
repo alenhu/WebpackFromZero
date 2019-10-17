70fd008b22b28666bc09bdde60539516666c0998
alenhu <huhuiyan@star-net.cn>
2019-10-17 19:35:31 +0800

    新增VUE

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,22 +1,35 @@
 /* eslint-disable no-console */
-import { keys } from 'lodash'
-alert('i am index.js,on dev!!!')
-console.log([1, 2, 3].findIndex(x => x === 4))
+// import { keys } from 'lodash'
+import Vue from 'vue'
+import App from './main.vue'
+// alert('i am index.js,on dev!!!')
+// console.log([1, 2, 3].findIndex(x => x === 4))
 
-console.log('abc'.padStart(10))
+// console.log('abc'.padStart(10))
 
-const alertMe = (msg) => {
-  console.log(msg)
-}
-class Robot {
-  constructor (msg) {
-    this.message = msg
-  }
+// const alertMe = (msg) => {
+//   console.log(msg)
+// }
+// class Robot {
+//   constructor (msg) {
+//     this.message = msg
+//   }
 
-  say () {
-    alertMe(this.message)
-  }
-}
-const marvin = new Robot('hello babelssss')
-marvin.say()
-console.log(keys(marvin))
+//   say () {
+//     alertMe(this.message)
+//   }
+// }
+// const marvin = new Robot('hello babelssss')
+// marvin.say()
+// console.log(keys(marvin))
+Vue.config.productionTip = false
+// eslint-disable-next-line no-new
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
---
+++ src/frontend/main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  {{content}}
+  </div>
+</template>
+<script>
+console.log('vue 1st!')
+export default {
+  name: 'App',
+  data (){
+    return {
+      content: 'I am vue!!!'
+    }
+  },
+  mounted() {
+    console.log('mouted')
+  },
+}
+</script>
